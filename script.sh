b179fb11894742998a04482bb387ed8cf0651bb3
Samir Panda <samir.panda91@gmail.com>
2025-02-07 19:40:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Container, TextField, Select, MenuItem, FormControl, InputLabel, Button, Typography } from "@mui/material";
+import {
+  Container, TextField, Select, MenuItem, FormControl, InputLabel, Button,
+  Typography, Card, CardContent
+} from "@mui/material";
 import axios from "axios";
 
 const App = () => {
@@ -9,7 +12,6 @@ const App = () => {
   const [options, setOptions] = useState([]);
 
   useEffect(() => {
-    // Fetch dropdown options from API
     axios.get("https://jsonplaceholder.typicode.com/users")
       .then((response) => {
         setOptions(response.data.map(user => ({ id: user.id, name: user.name })));
@@ -23,41 +25,67 @@ const App = () => {
   };
 
   return (
-    <Container maxWidth="sm" sx={{ mt: 5, p: 3, boxShadow: 3, borderRadius: 2 }}>
-      <Typography variant="h5" gutterBottom>React Form</Typography>
+    <Container maxWidth="sm" sx={{ mt: 5 }}>
+      <Card sx={{ boxShadow: 5, borderRadius: 3, p: 3, backgroundColor: "#f4f6f8" }}>
+        <CardContent>
+          <Typography variant="h5" textAlign="center" fontWeight="bold" gutterBottom>
+            React Form
+          </Typography>
 
-      <TextField 
-        fullWidth 
-        label="First Text Field" 
-        variant="outlined" 
-        value={text1} 
-        onChange={(e) => setText1(e.target.value)} 
-        margin="normal"
-      />
+          <TextField 
+            fullWidth 
+            label="First Text Field" 
+            variant="outlined" 
+            value={text1} 
+            onChange={(e) => setText1(e.target.value)} 
+            margin="normal"
+            sx={{ backgroundColor: "white", borderRadius: 1 }}
+          />
 
-      <TextField 
-        fullWidth 
-        label="Second Text Field" 
-        variant="outlined" 
-        value={text2} 
-        onChange={(e) => setText2(e.target.value)} 
-        margin="normal"
-      />
+          <TextField 
+            fullWidth 
+            label="Second Text Field" 
+            variant="outlined" 
+            value={text2} 
+            onChange={(e) => setText2(e.target.value)} 
+            margin="normal"
+            sx={{ backgroundColor: "white", borderRadius: 1 }}
+          />
 
-      <FormControl fullWidth margin="normal">
-        <InputLabel>Select an Option</InputLabel>
-        <Select value={dropdown} onChange={(e) => setDropdown(e.target.value)}>
-          {options.map((option) => (
-            <MenuItem key={option.id} value={option.name}>
-              {option.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Select an Option</InputLabel>
+            <Select
+              value={dropdown}
+              onChange={(e) => setDropdown(e.target.value)}
+              sx={{ backgroundColor: "white", borderRadius: 1 }}
+            >
+              {options.map((option) => (
+                <MenuItem key={option.id} value={option.name}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
-      <Button variant="contained" color="primary" fullWidth onClick={handleSubmit} sx={{ mt: 2 }}>
-        Submit
-      </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={handleSubmit}
+            sx={{
+              mt: 2,
+              py: 1,
+              fontSize: "1rem",
+              fontWeight: "bold",
+              textTransform: "none",
+              transition: "0.3s",
+              "&:hover": { backgroundColor: "#1976D2" },
+            }}
+          >
+            Submit
+          </Button>
+        </CardContent>
+      </Card>
     </Container>
   );
 };
